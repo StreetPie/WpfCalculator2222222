a270ab4fc8e0589c3ba32cdd74ad4961d84a11fa
OparinDG <OparinDG@310D-9>
2023-10-27 14:10:22 +0700

    Добавьте файлы проекта.

---
+++ SumSeriesLibrary/Calculation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SumSeriesLibrary
+{
+    public class Calculation
+    {
+        public double CalculateSumSeries(double x, double eps)
+        {
+            double sum = 0;
+            double term = 1;
+            int n = 0;
+
+            while (Math.Abs(term) >= eps)
+            {
+                sum += term;
+                n++;
+                term *= x / n;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ WpfCalculator2222222/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SumSeriesLibrary;
+
+namespace WpfCalculator2222222
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Calculation calculator = new Calculation();
+
+            double x = double.Parse(XTextBox.Text); 
+            double eps = double.Parse(EpsTextBox.Text); 
+
+            double result = calculator.CalculateSumSeries(x, eps);
+
+            ResultListBox.Items.Add($"Результат вычисления при x = {x} и e = {eps}. Ответ: {result}");
+        }
+    }
+}
